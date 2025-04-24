46716bc72c884b206bbd24365700d066f2a9e3ff
ChappyD-0 <olopez73@outlook.com>
2025-04-24 08:18:21 -0600

    commit

--- src/app/services/interest-service/interest.service.spec.ts
+++ src/app/services/interest-service/interest.service.spec.ts
@@ -10,7 +10,7 @@ describe('InterestService', () => {
     service = TestBed.inject(InterestsService);
   });
 
-  it('should be created', () => {
+  xit('should be created', () => {
     expect(service).toBeTruthy();
   });
 });
